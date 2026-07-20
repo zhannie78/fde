8ad238a2e4bfad3969e4e2712e3986e2a7fcdb2c
Annie <62417811+zhannie78@users.noreply.github.com>
2026-07-20 22:51:09 +0200

    feat(06-05): add /about to the sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from "next";
 
 import { siteConfig } from "@/config/site";
 
-const routes = ["", "/book"];
+const routes = ["", "/book", "/about"];
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = `https://${siteConfig.domain}`;
